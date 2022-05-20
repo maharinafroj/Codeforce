43fa3fa332efdb97420e45d8408ce473effe7620
maharin_afroj <74603459+maharinafroj@users.noreply.github.com>
2022-05-20 23:11:54 +0600

    Create 1684A-Digitminimization.cpp

---
+++ 1684A-Digitminimization.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+#define sesh_to return 0;
+#define HA cout << "YES\n";
+#define Ha cout<<"Yes\n";
+#define ha cout<<"yes\n";
+#define NA cout << "NO\n";
+#define Na cout<<"No\n";
+#define na cout<<"no\n";
+using namespace std;
+#define optimise() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+bool isprime(ll n)
+{
+     for(ll i = 2; i*i <= n ; i++)
+     {
+        if(n%i == 0) return false;
+
+     }
+
+     return true;
+}
+bool palindrome(string s)
+{
+    for(ll i=0,j=s.size()-1;i<j;i++,j--)
+    {
+        if(s[i]!=s[j])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+  optimise();
+  ll i,t,n,a,b,c,d,u=0,res=0,count=0;
+  cin>>t;
+  while(t--)
+    {
+
+  string s;
+    cin>>s;
+    vector<int>v;
+    for(i=0;i<s.size();i++)
+    {
+        v.push_back(s[i]-'0');
+
+    }
+    sort(v.begin(),v.end());
+
+        if(s.size()==2)
+      cout<<s[1]<<endl;
+
+        else
+
+            cout<<v[0]<<endl;
+
+    }
+   sesh_to
+  }
+
+
+
