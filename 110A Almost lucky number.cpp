6d07499b508324aa7b0809d62a9dfc033cf775d8
maharin_afroj <74603459+maharinafroj@users.noreply.github.com>
2022-06-13 22:35:43 +0600

    Create 110A Almost lucky number.cpp

---
+++ 110A Almost lucky number.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int count=0;
+    bool flag=false;
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]=='4'||s[i]=='7')
+        {
+         count++;
+
+        }
+
+    }
+    if(count==4||count==7)
+    {
+     cout<<"YES"<<endl;
+    }
+    else
+    {
+
+   cout<<"NO"<<endl;
+
+    }
+}
